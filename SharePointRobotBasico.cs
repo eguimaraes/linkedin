8159bab883de55ac66245a897da438885d1eda42
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-03-03 12:21:31 -0300

    Create SharePointRobotBasico.cs

---
+++ SharePointRobotBasico.cs
@@ -0,0 +1,34 @@
+using System;
+
+using Microsoft.SharePoint;
+
+namespace HelloSharePoint {
+
+class Program {
+
+static void Main() {
+
+const string siteUrl = "http://intranet.ekis.com.br";|
+
+using (SPSite siteCollection = new SPSite(siteUrl)) {
+
+SPWeb site = siteCollection.RootWeb;
+
+foreach (SPList list in site.Lists) {
+
+if (!list.Hidden) {
+
+Console.WriteLine(list.Title);
+
+}
+
+}
+
+}
+
+}
+
+}
+
+}
+
