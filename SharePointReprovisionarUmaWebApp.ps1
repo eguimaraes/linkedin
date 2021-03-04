2f89a14323ecc181707dfa98aee8b5ee87e20192
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-03-04 15:56:21 -0300

    Update SharePointReprovisionarUmaWebApp.ps1

--- SharePointReprovisionarUmaWebApp.ps1
+++ SharePointReprovisionarUmaWebApp.ps1
@@ -2,4 +2,5 @@ $appname=http://appnameUrl
 $app=Get-SPWebApplication $appname
 $app.Provision()
 $solutions=get-spsolution
-foreach ($sol in $solution){install-spsolution $sol.name -webapplication $appname}
+foreach ($sol in $solutions)
+{install-spsolution $sol.name -webapplication $appname -GACDeployment -Force -Confirm:$FALSE}
