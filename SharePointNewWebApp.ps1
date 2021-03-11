a2a9d25f3f8636bc6c2099148e2dc83f09e73e6b
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-03-11 19:13:56 -0300

    Update SharePointNewWebApp.ps1

--- SharePointNewWebApp.ps1
+++ SharePointNewWebApp.ps1
@@ -1,1 +1,14 @@
+$nome
+
+$porta
+
+$hh=
+
+$appool
+
+$appConta=
+
+
+
+New-SPWebApplication -Name "Contoso Internet Site" -Port 80 -HostHeader sharepoint.contoso.com -URL "http://www.contoso.com" -ApplicationPool "ContosoAppPool" -ApplicationPoolAccount (Get-SPManagedAccount "DOMAIN\wa")
 
